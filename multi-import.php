2d8b9b4775fab7f90ff71e786f9606e2409dfe43
Emil Mohamed <emil@2c-studio.com>
2014-08-07 15:01:31 +0300

    Initial working version.

--- multi-import.php
+++ multi-import.php
@@ -1,198 +1,153 @@
-a<?php
+<?php
+// Deals with queries separation process. The code is borrowed from phpminiadmin(http://phpminiadmin.sourceforge.net/),
+// but has been cleaned up a little.
 
-class ShuttleSqlDumpParserToken {
-	public $name;
-	public $start_pos, $end_pos;
-	public $length;
+class ShuttleSQLDumpFileSeparator {
+	static function separate_queries($file_name, $callback) {
+		$query = '';
+		$opening_token = '';
+		$is_comment = false;
 
-	function __construct($name, $position) {
-		$this->name = $name;
-		$this->length = strlen($name);
-		$this->start_pos = $position;
-		$this->end_pos = $this->start_pos + $this->length;
-	}
-}
+		while ($input_string = self::get_next_chunk($file_name)) {
+			$opening_pos =- strlen($opening_token);
+			$curren_pos = 0;
+			$i = strlen($input_string);
 
-class ShuttleSqlDumpParser {
-	/**
-	 * Location of the dump .sql file. Currently the class doesn't support .sql.gz files.
-	 */
-	private $file; 
+			while ( $i-- ) {
+				if ( $opening_token ) {
+					list($closing_token, $closing_pos) = self::get_closing_token(
+						$input_string,
+						$opening_pos + strlen($opening_token),
+						$opening_token
+					);
+					
+					if ( $closing_token ) {
+						if ( $opening_token === '--' || $opening_token=='#' || $is_comment ) {
+							$query .= substr(
+								$input_string,
+								$curren_pos,
+								$opening_pos - $curren_pos
+							);
+						} else {
+							$query .= substr(
+								$input_string,
+								$curren_pos,
+								$closing_pos + strlen($closing_token) - $curren_pos
+							);
+						}
 
-	/**
-	 * The query that is being extracted from the dump now. 
-	 */
-	private $query;
-
-	function __construct($file) {
-		$this->file = $file;
-	}
-
-	function split($callback) {
-
-	}
-}
-
-define('DEFAULT_READ_BUFFER_SIZE', 65536);
-
-function do_multi_sql($file_name, $callback="do_one_sql") {
-	$query = '';
-	$opening_token = '';
-	$is_comment_context = false;
-
-	while ($input_string = get_next_chunk($file_name)) {
-		$opening_pos =- strlen($opening_token);
-		$current_pos = 0;
-		$i = strlen($input_string);
-
-		while ( $i-- ) {
-			if ( $opening_token ) {
-				$closing_token = get_closing_token(
-					$input_string,
-					$opening_pos + strlen($opening_token),
-					$opening_token
-				);
-				
-				if ( $closing_token !== null ) {
-					if ( $opening_token === '--' || $opening_token=='#' || $is_comment_context ) {
-						$query .= substr(
-							$input_string,
-							$current_pos,
-							$opening_pos - $current_pos
-						);
+						$curren_pos = $closing_pos + strlen($closing_token);
+						$opening_token = '';
+						$opening_pos = 0;
 					} else {
-						$query .= substr(
-							$input_string,
-							$current_pos,
-							$closing_token->end_pos - $current_pos
-						);
+						$query .= substr($input_string, $curren_pos);
+						break;
 					}
 
-					$current_pos = $closing_token->end_pos;
-
-					$opening_token = '';
-					$opening_pos = 0;
 				} else {
-					$query .= substr($input_string, $current_pos);
-					break;
-				}
+					list($opening_token, $opening_pos) = self::get_opening_token($input_string, $curren_pos);
 
-			} else {
-				list($opening_token, $opening_pos) = get_opening_token($input_string, $current_pos);
+					if ($opening_token === ';') {
+						$query .= substr(
+							$input_string,
+							$curren_pos,
+							$opening_pos - $curren_pos + 1
+						);
 
-				if ($opening_token === ';') {
-					$query .= substr(
-						$input_string,
-						$current_pos,
-						$opening_pos - $current_pos + 1
-					);
+						call_user_func($callback, $query);
 
-					call_user_func($callback, $query);
+						$query = '';
+						$curren_pos = $opening_pos + strlen($opening_token);
+						$opening_token = '';
+						$opening_pos = 0;
 
-					$query = '';
-					$current_pos = $opening_pos + strlen($opening_token);
-					$opening_token = '';
-					$opening_pos = 0;
-
-				} elseif(!$opening_token) {
-					$query .= substr($input_string, $current_pos);
-					break;
-				} else {
-					if ($opening_token === '/*' && substr($input_string, $opening_pos, 3) !== '/*!') {
-						$is_comment_context = true;
+					} elseif(!$opening_token) {
+						$query .= substr($input_string, $curren_pos);
+						break;
 					} else {
-						$is_comment_context = false;
+						if ($opening_token === '/*' && substr($input_string, $opening_pos, 3) !== '/*!') {
+							$is_comment = true;
+						} else {
+							$is_comment = false;
+						}
 					}
 				}
 			}
 		}
+
+		if ($query) {
+			call_user_func($callback, $query);
+			$query = '';
+		}
+
+		return 1;
 	}
 
-	if ($query) {
-		call_user_func($callback, $query);
-		$query = '';
-	}
-
-	return true;
-}
-
-//read from insql var or file
-function get_next_chunk($file_name, $buffer=DEFAULT_READ_BUFFER_SIZE) {
-	static $file_handle;
-
-	if (!$file_handle) {
-		$file_handle = fopen($file_name, "r+b");
+	//read from insql var or file
+	static function get_next_chunk($file_name, $buffer=65536) {
+		static $file_handle;
 
 		if (!$file_handle) {
-			throw new Exception("Can't open $file_name file. ");
-		}
-	}
+			$file_handle = fopen($file_name, "r+b");
 
-	return fread($file_handle, $buffer);
-}
-
-function get_opening_token($input_string, $pos){
-	$opening_token = null;
-	$opening_pos = null;
-
-	if ( preg_match('~(\/\*|^--|(?<=\s)--|#|\'|"|;)~', $input_string, $matches, PREG_OFFSET_CAPTURE, $pos) ) {
-		$opening_token = $matches[1][0];
-		$opening_pos = $matches[1][1];
-	}
-
-	return array($opening_token, $opening_pos);
-}
-
-function get_closing_token($input_string, $pos, $opening_token) {
-	$closing_tokens = array(
-		// opening token => closing token token
-		'\''  => '(?<!\\\\)\'|(\\\\+)\'',
-		'"'   => '(?<!\\\\)"',
-		'/*'  => '\*\/',
-		'#'   => '[\r\n]+',
-		'--'  => '[\r\n]+',
-	);
-
-	if (!isset($closing_tokens[$opening_token])) {
-		return null;
-	}
-	$closing_token_regex = $closing_tokens[$opening_token];
-
-	if ( ! preg_match('~(' . $closing_token_regex . ')~', $input_string, $matches, PREG_OFFSET_CAPTURE, $pos ) ) {
-		return null;
-	}
-	$closing_token_name = $matches[1][0];
-	$closing_pos = $matches[1][1];
-	
-	$closing_token = new ShuttleSqlDumpParserToken($closing_token_name, $closing_pos);
-
-
-	if (isset($matches[2][0])) {
-		$sl = strlen($matches[2][0]);
-
-		if ($opening_token === "'" && $sl) {
-			if ($sl % 2) {
-				$closing_token = get_closing_token(
-					$input_string,
-					$closing_token->end_pos,
-					$opening_token
-				);
-			} else {
-				$closing_pos += strlen($closing_token) - 1;
-				$closing_token = "'";
+			if (!$file_handle) {
+				throw new Exception("Can't open [$file_name] file. ");
 			}
 		}
+
+		return fread($file_handle, $buffer);
 	}
 
-	return $closing_token;
-}
+	static function get_opening_token($input_string, $pos){
+		$opening_token = null;
+		$opening_pos = null;
 
-function do_one_sql($query){
-	static $query_counter = 1;
-	$query = trim($query);
+		if ( preg_match('~(\/\*|^--|(?<=\s)--|#|\'|"|;)~', $input_string, $matches, PREG_OFFSET_CAPTURE, $pos) ) {
+			$opening_token = $matches[1][0];
+			$opening_pos = $matches[1][1];
+		}
 
-	echo trim("Query $query_counter: $query\n\n\n-----\n\n\n");
-	$query_counter++;
+		return array($opening_token, $opening_pos);
+	}
 
-	return 1;
+	static function get_closing_token($input_string, $pos, $opening_token) {
+		$closing_characters = array(
+			// opening character => closing character regexes
+			'\''  => '(?<!\\\\)\'|(\\\\+)\'',
+			'"'   => '(?<!\\\\)"',
+			'/*'  => '\*\/',
+			'#'   => '[\r\n]+',
+			'--'  => '[\r\n]+',
+		);
+
+		$closing_token = null;
+		$closing_pos = null;
+		if (!isset($closing_characters[$opening_token])) {
+			return array($closing_token, $closing_pos);
+		}
+		$closing_character_regex = $closing_characters[$opening_token];
+
+		if (preg_match('~(' . $closing_character_regex . ')~', $input_string, $matches, PREG_OFFSET_CAPTURE, $pos ) ) {
+			$closing_token = $matches[1][0];
+			$closing_pos = $matches[1][1];
+			if (isset($matches[2][0])) {
+				$sl = strlen($matches[2][0]);
+
+				if ($opening_token=="'" && $sl) {
+					if ($sl % 2) {
+						list($closing_token, $closing_pos) = self::get_closing_token(
+							$input_string,
+							$closing_pos + strlen($closing_token),
+							$opening_token
+						);
+					} else {
+						$closing_pos += strlen($closing_token) - 1;
+						$closing_token = "'";
+					}
+				}
+			}
+		}
+
+		return array($closing_token, $closing_pos);
+	}
 }
--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
 include ("multi-import.php");
+
 $start = microtime(true);
-do_multi_sql("test.sql");
+
+ShuttleSQLDumpFileSeparator::separate_queries("test.sql", function ($query) {
+	echo "--- $query --- \n\n";
+});
+
 echo "Took: " . sprintf('%0.2f', microtime(true) - $start);
